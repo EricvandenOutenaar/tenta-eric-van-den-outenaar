1e14a68df7d83303e55dd128dda748fa9f30d59d
EricvandenOutenaar <55479562+EricvandenOutenaar@users.noreply.github.com>
2020-09-14 14:48:01 +0200

    added theme and global styles

---
+++ src/themes/theme.js
@@ -0,0 +1,37 @@
+export default {
+  fonts: {
+    main: "Roboto, Helvetica, Arial, sans-serif",
+  },
+  colors: {
+    primaryMain: "#304ffe",
+    primaryLight: "#7a7cff",
+    primaryDark: "#0026ca",
+    secondaryMain: "#fedf30",
+    secondaryLight: "#ffff69",
+    secondarDark: "#c7ae00",
+    onPrimary: "#ffffff",
+    onSecondary: "#000000",
+    error: "#b00020",
+    background: "#ffffff",
+    onBackground: "#b00020",
+  },
+  breakpoints: {
+    mobile: "only screen and (min-width: 50rem)",
+  },
+  spacings: {
+    xxSmall: ".25rem",
+    xSmall: ".5rem",
+    small: "1rem",
+    medium: "2rem",
+    large: "3rem",
+    xLarge: "4rem",
+    xxLarge: "6rem",
+  },
+  animations: {
+    button: "box-shadow 0.3s ease",
+    link: "color 0.2s ease",
+  },
+  shadows: {
+    shadow1: "0px 5px 20px rgba(30, 30, 31, 0.05)",
+  },
+};
